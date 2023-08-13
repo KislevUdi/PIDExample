08903fc2d0b7d70825aead67d9d8437d164f9dd2
1HyperZ <omerzur06@gmail.com>
2023-08-13 17:54:49 +0300

    Added printbill

---
+++ src/main/java/frc/robot/commands/printBill.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class printBill extends CommandBase{
+
+    @Override
+    public void execute() {
+        System.out.println("Bill");
+    }
+    
+}
